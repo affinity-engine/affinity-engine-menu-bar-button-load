cd1b5aa55f30c037467eadce02a3aba92e4d86b7
null null null <null@affinitygamescollective.org>
2017-09-13 13:22:06 -0400

    prevent default config conflicts with user config

--- addon/affinity-engine/configs/menu-bar/button/load.js
+++ addon/affinity-engine/configs/menu-bar/button/load.js
@@ -1,13 +1,15 @@
 export default {
   priority: 2,
-  component: {
-    menuBar: {
-      button: {
-        load: {
-          attrs: {
-            header: 'affinity-engine.menu-bar.buttons.load.header',
-            icon: {
-              type: 'download'
+  default: {
+    component: {
+      menuBar: {
+        button: {
+          load: {
+            attrs: {
+              header: 'affinity-engine.menu-bar.buttons.load.header',
+              icon: {
+                type: 'download'
+              }
             }
           }
         }
